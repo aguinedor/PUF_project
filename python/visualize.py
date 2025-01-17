e445eba09eb3e09d7db1e0ac80e27db5317b19f9
adrien.guinedor <adrien@Pc2-Adrien.>
2025-01-17 16:05:00 +0100

    Updated heatmap & ploting functions

--- python/visualize.py
+++ python/visualize.py
@@ -10,7 +10,6 @@ import argparse
 #                             DATA AREA
 
 
-
 # parse_line()
 # - input time spent in ms
 # - return response, frequency1, frequency2
@@ -25,15 +24,14 @@ def parse_line(line, time_spent=10):
     return response, frequency1, frequency2
 
 
-
 # parse_file()
-# - bits_dict        -> return [index]{'0' = count 0, '1' = count 1}
+# - bits_dict        -> return [index][bit0, bit1, bit2 ...]
 # - frequencies_dict -> return [index]{'freq1' = [resp0, resp1 ...], 'freq2' = [resp0, resp1 ...]}
 # -----------------------------------------------------------------------
 def parse_file(file_path, nb_ro = 256, time_spent=10):
     current_index    = 0
     frequencies_dict = [{'freq1': [], 'freq2': []}   for _ in range(nb_ro)]
-    bits_dict        = [{'0': 0, '1': 0} for _ in range(nb_ro)]
+    bits_dict        = [[] for _ in range(nb_ro)]
 
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -47,16 +45,12 @@ def parse_file(file_path, nb_ro = 256, time_spent=10):
             response, frequency1, frequency2 = parse_line(line, time_spent)
             frequencies_dict[current_index]['freq1'].append(frequency1)
             frequencies_dict[current_index]['freq2'].append(frequency2)
-            if response == 1:
-                bits_dict[current_index]['1'] += 1
-            else:
-                bits_dict[current_index]['0'] += 1
+            bits_dict[current_index].append(response)
 
             current_index += 1
             if current_index == nb_ro:
                 current_index = 0
     return bits_dict, frequencies_dict
-
 
 
 # get_mean_values()
@@ -76,9 +70,9 @@ def get_mean_values(frequencies_dict):
 
 
 
+
 # ----------------------------------------------------------------------------------------
 #                             PLOTTING AREA
-
 
 
 # Plot 2D graph of frequencies heatmap
@@ -113,7 +107,6 @@ def plot_2D_heatmap(frequencies_dict):
     ax.invert_yaxis()
 
 
-
 # Plot 2D graph of frequencies differences
 # -----------------------------------------------------------------------
 def plot_2D_heatmap_diff_freq(frequencies_dict):
@@ -146,110 +139,22 @@ def plot_2D_heatmap_diff_freq(frequencies_dict):
     ax.invert_yaxis()
 
 
-
-
-
-
+# Gap Distribution
+# - precision -> arrondit à un chiffre après la virgule, precision=1 arrondit à l’unité
+# - round     -> indique la division en fréquence à réaliser default 1Mhz
 # -----------------------------------------------------------------------
-# HISTOGRAM
-def plot_histogram(bit_counts):
-    # Extraire les valeurs pour les histogrammes
-    zeros = [bit_count['0'] for bit_count in bit_counts]
-    ones  = [bit_count['1'] for bit_count in bit_counts]
-    # Indices
-    indices = np.arange(len(bit_counts))
+def plot_gap_distribution(frequencies_dict, precision, div_freq= 1000000):
 
-    # Largeur des barres
-    bar_width = 1  # Réduire la largeur des barres pour mieux visualiser les différences de couleurs
-    # Définir les couleurs
-    zero_color = 'green'
-    one_color = 'red'
-    # Créer la figure
-    plt.figure(3, figsize=(12, 6))
-
-    # Dessiner les barres
-    for idx in indices:
-        if zeros[idx] > 0 and ones[idx] > 0:
-            # Si les deux valeurs sont non nulles, dessiner deux barres séparées
-            plt.bar(idx, zeros[idx], bar_width, label='0' if idx == 0 else "", color=zero_color)
-            plt.bar(idx, ones[idx] , bar_width, label='1' if idx == 0 else "", color=one_color, bottom=zeros[idx])
-        elif zeros[idx] > 0:
-            plt.bar(idx, zeros[idx], bar_width, color='white')
-        elif ones[idx] > 0:
-            plt.bar(idx, ones[idx] , bar_width, color='white')
-
-    # Ajouter des légendes et des titres
-    plt.xlabel('Index des bits')
-    plt.ylabel('Nombre d\'occurrences')
-    plt.title(f'Histogramme des occurrences de 0 {zero_color} et 1 {one_color} pour chaque bit flip')
-    plt.legend()
-
-# -----------------------------------------------------------------------
-# FREQUENCY DISTRIBUTION
-def plot_frequency_distribution(file_path, precision):
-
-    #Dictionnaire pour compter les occurrences de fréquences
-    freq_counter = Counter()
-
-    #Lecture des données du fichier
-    with open(file_path, 'r') as file:
-        for line in file:
-            #Parser la ligne pour en extraire les données
-            _, freq1, freq2 = parse_line(line)
-
-            #Conversion des fréquences de Hz à MHz
-            freq1_in_MHz = freq1/1000000
-            freq2_in_MHz = freq2/1000000
-
-            #Arrondir les fréquences à la précision donnée
-            #precision=0.1 arrondit à un chiffre après la virgule, precision=1 arrondit à l’unité
-            rounded_freq1 = round(freq1_in_MHz, precision)
-            rounded_freq2 = round(freq2_in_MHz, precision)
-
-            #Ajouter les fréquences arrondies au compteur
-            freq_counter[rounded_freq1] += 1
-            freq_counter[rounded_freq2] += 1
-
-    #Conversion du compteur en liste triée
-    sorted_frequencies = sorted(freq_counter.items())  # [(fréquence, occurrences), ...]
-
-    #Séparation des fréquences et des occurrences
-    frequencies = [item[0] for item in sorted_frequencies]
-    occurrences = [item[1] for item in sorted_frequencies]
-
-    # Créer la figure
-    plt.figure(2, figsize=(12, 6))
-    plt.bar(frequencies, occurrences, color='blue', alpha=0.7, label="Occurence des fréquences")
-
-    # Ajouter les labels et le titre
-    plt.xlabel('Fréquences (en MHz)')
-    plt.ylabel('Nombre d\'occurrences')
-    plt.title('Distribution des fréquences (arrondies à {} MHz)'.format(precision))
-    plt.legend()
-    plt.grid(True)
-
-
-# -----------------------------------------------------------------------
-# GAP DISTRIBUTION
-def plot_gap_distribution(file_path, precision):
-
+    nb_ro = len(frequencies_dict)
+    nb_it = len(frequencies_dict[0]['freq1'])
     #Dictionnaire pour compter les occurrences de gap entre 2 fréquences
     gap_counter = Counter()
 
-    #Lecture des données du fichier
-    with open(file_path, 'r') as file:
-        for line in file:
-            #Parser la ligne pour en extraire les données
-            _, freq1, freq2 = parse_line(line)
+    for i in range(nb_ro):
+        for j in range(nb_it):
 
-            #Conversion des fréquences de Hz à MHz
-            gap = freq1/1000000 - freq2/1000000
-
-            #Arrondir les gaps à la précision donnée
-            #precision=0.1 arrondit à un chiffre après la virgule, precision=1 arrondit à l’unité
+            gap = frequencies_dict[i]['freq1'][j]/div_freq - frequencies_dict[i]['freq2'][j]/div_freq
             rounded_gap = round(gap, precision)
-
-            #Ajouter les gaps arrondis au compteur
             gap_counter[rounded_gap] += 1
 
     #Conversion du compteur en liste triée
@@ -260,8 +165,8 @@ def plot_gap_distribution(file_path, precision):
     occurrences = [item[1] for item in sorted_gaps]
 
     # Créer la figure
-    plt.figure(1, figsize=(12, 6))
-    plt.bar(gaps, occurrences, color='orange', alpha=0.7, label="Occurence des gaps")
+    plt.figure(figsize=(12, 6))
+    plt.bar(gaps, occurrences, width=1, color='orange', alpha=0.7, label="Occurence des gaps")
 
     # Ajouter les labels et le titre
     plt.xlabel('Différences de fréquences entre les 2 compteurs (en MHz)')
@@ -271,79 +176,137 @@ def plot_gap_distribution(file_path, precision):
     plt.grid(True)
 
 
-
+# Histrogram
+# - visualization of bits 0 or 1
 # -----------------------------------------------------------------------
-# Main program to parse data
-def analyse_data(file_path):
+def plot_histogram(bits_dict, white='yes'):
+    nb_cols  = len(bits_dict)
+    nb_lines = len(bits_dict[0])
 
-    print("Start processing the file...")
+    Z = np.zeros((nb_lines, nb_cols))
+    x = np.arange(nb_cols + 1)
+    y = np.arange(nb_lines + 1)
 
-    # Lire le fichier en mode ligne par ligne
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    for col in range(nb_cols):
 
-        nb_ro = 256
+        if sum(bits_dict[col]) == nb_lines or sum(bits_dict[col]) == 0:
+            display = 0
+        else:
+            display = 1
 
-        bit_counts    = [{'0': 0, '1': 0} for _ in range(nb_ro)]
-        current_index = 0
-        bits_flip     = [] #tableau pour les bits flips
-        bits_normal   = []
-        processed_lines = set()
-        largest_gap = 0
+        for line in range(nb_lines):
+            if white == 'no':
+                Z[line, col] = bits_dict[col][line]
+                continue
+            if white == 'yes':
+                if display:
+                    Z[line, col] = bits_dict[col][line]
+                else:
+                    Z[line, col] = 0.5
+                continue
 
-        # Vérifier si le nombre de lignes est un multiple du nombre de RO activées
-        if len(lines) % nb_ro != 0:
-            raise ValueError("Le fichier doit contenir un nombre de lignes multiple de " + nb_ro)
+    # Création de la figure et de l'axe
+    fig, ax = plt.subplots(figsize=(12, 9))
+    # Affichage avec pcolormesh
+    mesh = ax.pcolormesh(x, y, Z, shading='flat', cmap='viridis', vmin=Z.min(), vmax=Z.max())
+    # Ajout d'une barre de couleur
+    plt.colorbar(mesh, ax=ax, label="Frequency Value")
 
-        # Traiter chaque ligne
-        for line in lines:
-            response, frequency1, frequency2 = parse_line(line)
-
-            # Incrémenter le compteur correspondant (0 ou 1)
-            if response == 0:
-                bit_counts[current_index]['0'] += 1
-            elif response == 1:
-                bit_counts[current_index]['1'] += 1
-
-            difference = frequency1 - frequency2
-            if bit_counts[current_index]['0'] > 0 and bit_counts[current_index]['1'] > 0: #and current_index not in processed_lines:
-                bits_flip.append({
-                    'index': current_index,
-                    'difference': difference,
-                    'response': response
-                })
-                #processed_lines.add(current_index)
-                if difference > largest_gap:
-                    largest_gap = difference
-            else:
-                bits_normal.append({
-                    'index': current_index,
-                    'difference': difference,
-                    'response': response
-                })
-            current_index += 1
-            if current_index == nb_ro:
-                current_index = 0
+    # Configuration de l'axe
+    ax.set_title("Heatmap of response with iteration")
+    ax.set_xlabel("Colonnes - index X response")
+    ax.set_ylabel("Lignes - index Y iteration")
 
 
-        bits_flip   = sorted(bits_flip, key= lambda x: x['index'])
-        bits_normal = sorted(bits_normal, key= lambda x: x['index'])
 
-    print("Processing completed.")
-    #print(bits_flip)
-    for entry in bits_flip:
+# Frequency distribution
+# -----------------------------------------------------------------------
+def plot_frequency_distribution(frequencies_dict):
+    nb_ro = len(frequencies_dict)
+    nb_it = len(frequencies_dict[0]['freq1'])
+    freq_counter = Counter()
+
+    for idx in range(nb_ro):
+        for ite in range(nb_it):
+
+            freq1_Mhz = frequencies_dict[idx]['freq1'][ite] / 1000000
+            freq2_MHz = frequencies_dict[idx]['freq2'][ite] / 1000000
+
+            freq1 = round(freq1_Mhz, 1)
+            freq2 = round(freq2_MHz, 1)
+
+            freq_counter[freq1] += 1
+            freq_counter[freq2] += 1
+
+    #Conversion du compteur en liste triée
+    sorted_frequencies = sorted(freq_counter.items())  # [(fréquence, occurrences), ...]
+
+    #Séparation des fréquences et des occurrences
+    frequencies = [item[0] for item in sorted_frequencies]
+    occurrences = [item[1] for item in sorted_frequencies]
+
+    # Créer la figure
+    plt.figure(figsize=(12, 6))
+    plt.bar(frequencies, occurrences, color='blue', alpha=0.7, label="Occurence des fréquences")
+
+    # Ajouter les labels et le titre
+    plt.xlabel('Fréquences (en MHz)')
+    plt.ylabel('Nombre d\'occurrences')
+    plt.title('Distribution des fréquences (arrondies à 1 MHz)')
+    plt.legend()
+    plt.grid(True)
+
+
+
+
+# ----------------------------------------------------------------------------------------
+#                             TEXT PRINTING AREA
+
+
+# Bit flip detector
+# - take bits & frequencies as input
+# -----------------------------------------------------------------------
+def bit_flip_analysis(bits_dict, frequencies_dict):
+
+    nb_ro = len(frequencies_dict)
+    nb_it = len(frequencies_dict[0]['freq1'])
+
+    print(f"ro {nb_ro}  it {nb_it}")
+
+    #tableau multidimensionnel pour stocker les gaps de chaque index et chaque iteration
+    gap            = [[0.0 for _ in range(nb_it)] for _ in range(nb_ro)]
+    #tableau pour stocker la valeur max de gap pour un index
+    gap_max        = [0.0 for _ in range(nb_ro)]
+    #liste de dictionnaires pour stocker les différentes infos lors d'un flip (son index, son gap et sa réponse)
+    flip_info      = []
+
+    for idx in range(nb_ro):
+        for ite in range(nb_it):
+            if ite == 0:
+                continue
+
+            if bits_dict[idx][ite] != bits_dict[idx][ite-1]:
+                # Store frequencies gap when bit flip
+                gap[idx][ite] = abs(frequencies_dict[idx]['freq1'][ite] - frequencies_dict[idx]['freq2'][ite])
+                flip_info.append({ 'index': idx, 'iteration': ite, 'difference': gap[idx][ite], 'response': bits_dict[idx][ite] })
+
+                if gap[idx][ite] > gap[idx][ite-1]:
+                    # Actualize new max gap value
+                    gap_max[idx] = gap[idx][ite]
+
+    largest_gap = max(gap_max)
+    flip_info = sorted(flip_info, key= lambda x: x['index'])
+
+    for entry in flip_info:
         print(entry)
-
-    #for entry in bits_normal:
-        #print(entry)
-    print("The largest gap for a bit flip is:", largest_gap)
-    plot_gap_distribution(file_path, 1)
-    plot_frequency_distribution(file_path, 1)
-    plot_histogram(bit_counts)
-    # Afficher le graphique
-    plt.show()
+    print("The largest gap for a bit flip is: ", largest_gap)
 
 
+
+
+
+# ----------------------------------------------------------------------------------------
+#                                    MAIN AREA
 
 
 def main():
@@ -356,14 +319,17 @@ def main():
 
     # Parse file
     bits_dict, frequencies_dict = parse_file(args.file, args.ring, args.time)
-
-    # grab means
     frequencies_mean = get_mean_values(frequencies_dict)
 
-    # Plot graphs
+    # 1 - Plot text
+    bit_flip_analysis(bits_dict, frequencies_dict)
+
+    # 2 -Plot graphs
     plot_2D_heatmap_diff_freq(frequencies_mean)
     plot_2D_heatmap(frequencies_mean)
-
+    plot_gap_distribution(frequencies_dict, 1)
+    plot_histogram(bits_dict)
+    plot_frequency_distribution(frequencies_dict)
     plt.show()
 
 
